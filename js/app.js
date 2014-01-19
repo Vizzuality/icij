3c5e14d9916d8dd9ae9ca2c394690d36d84f9f6e
xavijam <xavijam@gmail.com>
2014-01-19 22:53:35 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+
+  $(function() {
+
+    var App = cdb.core.View.extend({
+
+      el: document.body,
+
+      events: {
+        'click #explore': '_toggleLanding'
+      },
+
+      initialize: function() {
+        this.model = new cdb.core.Model({ state: 'landing' });
+        this._initViews();
+      },
+
+      _initViews: function() {
+        // Map
+        this.map = new Map({
+          el:       this.$('div.map'),
+          template: this.$('#client_infowindow_template').html()
+        })
+        .bind("onHeadClick", function(){
+          this._toggleLanding();
+        }, this);
+
+        this.$("#background-image").fullscreenBackground();
+      },
+
+      _toggleLanding: function(e) {
+        if (e) e.preventDefault();
+        var state = this.model.get('state');
+        this.$('.landing')[state == "landing" ? 'fadeOut' : 'fadeIn' ]();
+        this.$('#background-image')[state == "landing" ? 'fadeOut' : 'fadeIn' ]();
+        this.model.set('state', state == "landing" ? 'map' : 'landing' );
+      }
+
+    });
+
+
+    cdb.init(function() {
+      window.app = new App();
+    });
+
+  });
---
+++ js/map.js
@@ -0,0 +1,81 @@
+
+  /**
+   *  Map js
+   */
+
+  var Map = cdb.core.View.extend({
+
+    options: {
+      zoom:     2,
+      center:   [0,0],
+      viz_url:  'https://icij.cartodb.com/api/v2/viz/bec546fa-61a8-11e3-858d-1bc60dd419cf/viz'
+    },
+
+    events: {
+      'click h2 a':           '_onHeadClick',
+      'click div.aside li a': '_onClick'
+    },
+
+    initialize: function() {
+      _.bindAll(this, '_onDone', '_onHeadClick');
+      this._initMap();
+    },
+
+    _initMap: function() {
+      cartodb.createVis(
+        this.$('#map')[0],
+        this.options.viz_url,
+        {
+          zoom: this.options.zoom,
+          center: this.options.center
+        }
+      )
+      .done(this._onDone)
+      .error(this._onError)
+    },
+
+    _onDone: function(vis,layers) {
+      var layer = layers[1];
+      layer.infowindow.set('offset', [224,0]);
+      
+      this.stories = layer.getSubLayer(2);
+      this.stories.infowindow.set({
+        template: this.options.template
+      })
+    },
+
+    _onError: function(e) {
+      console.log('There was a problem generating visualization from vis.json');
+    },
+
+    _toggleStories: function(bool) {
+      this.stories[bool ? 'show' : 'hide']();
+    },
+
+    _onHeadClick: function(e) {
+      if (e) e.preventDefault();
+      this.trigger('onHeadClick', this);
+    },
+
+    _onClick: function(e) {
+      var $a = $(e.target);
+      e.stopPropagation();
+      
+      if ($a.find('span.info').length > 0) {
+        e.preventDefault();
+        
+        // Select correct item
+        if (!$a.hasClass('selected'))
+          this._openItem($a);
+
+        // Toggle stories
+        this._toggleStories($a.hasClass('stories'))
+      }
+    },
+
+    _openItem: function($a) {
+      this.$('a').removeClass('selected');
+      $a.addClass('selected');
+    }
+
+  });
